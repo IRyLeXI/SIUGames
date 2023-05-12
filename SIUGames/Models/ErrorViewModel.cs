980492eee3ab7a153fd2d2ad4bfe70d6c082d8a6
IRyLeXI <maksgavalec@gmail.com>
2023-05-12 14:57:36 +0300

    Добавьте файлы проекта.

---
+++ SIUGames/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIUGames.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
