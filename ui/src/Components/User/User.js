82cb99b2b638703a2f7d829a98af6db68a493754
IRyLeXI <maksgavalec@gmail.com>
2023-05-28 16:52:00 +0300

    Added user page

--- ui/src/Components/User/User.js
+++ ui/src/Components/User/User.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import UserHeader from './UserHeader/UserHeader.js';
 import UserFavouriteGames from './UserBody/UserFavouriteGames.js';
+import "./User.css"
 
 const User = (props) =>{
     return (
-        <>
-            <UserHeader Image = {props.Image} UserName = {props.UserName} Description = {props.Description} />
-            <UserFavouriteGames />
-        </>
+        <div className='user'>
+            <div>
+                <UserHeader Image = {props.Image} UserName = {props.UserName} Description = {props.Description} />
+            </div>
+            <div>
+                <UserFavouriteGames />
+            </div>
+        </div>
     );
 }
+
+export default User;
